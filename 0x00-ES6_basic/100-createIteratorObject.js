4d87fb885b9cf516c5471ad8e8958dd9be0ce28b
Mbali Keteyi <keteyimbali@gmail.com>
2024-03-04 16:04:06 +0000

    0x00. ES6 Basics

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,24 +1,9 @@
 export default function createIteratorObject(report) {
-  let departments = Object.values(report.allEmployees);
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
-
-  return {
-    next() {
-      if (currentEmployeeIndex >= departments[currentDepartmentIndex].length) {
-        currentDepartmentIndex++;
-        currentEmployeeIndex = 0;
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
-
-      if (currentDepartmentIndex >= departments.length) {
-        return { done: true };
-      }
-
-      return {
-        done: false,
-        value: departments[currentDepartmentIndex][currentEmployeeIndex++]
-      };
     }
-  };
+  }());
 }
-
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,20 +1,8 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {
-    ...employeesList.reduce((acc, employee) => {
-      const department = employee.department;
-      if (!acc[department]) {
-        acc[department] = [];
-      }
-      acc[department].push(employee.name);
-      return acc;
-    }, {})
-  };
-
   return {
-    allEmployees,
-    getNumberOfDepartments: function() {
-      return Object.keys(allEmployees).length;
-    }
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
   };
 }
-
